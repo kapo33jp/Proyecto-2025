dee96c2073e299240cc9420f38b16a3c9c4b7fd7
Karim <45132886+kapo33jp@users.noreply.github.com>
2025-10-30 12:17:52 -0300

    w

--- html/Main.php
+++ html/Main.php
@@ -1,19 +1,3 @@
-<?php
-
-    session_start();
-    error_reporting(0);
-
-    $validar = $_SESSION['user_idusuario'];
-    $validar = $_SESSION['user_email'];
-    $validar = $_SESSION['user_idrol'];
-
-    if ($validar == null || $validar = '') {
-        header("location: ../html/login.html");
-        die();
-    }
-
-    ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
