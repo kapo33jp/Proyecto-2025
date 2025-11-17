a7bb154182cae3fc82a6db0aded6eb3627d8e016
Karim <45132886+kapo33jp@users.noreply.github.com>
2025-11-17 15:17:06 -0300

    Update index.php

--- Admin-Dashboard/index.php
+++ Admin-Dashboard/index.php
@@ -158,6 +158,11 @@
                                 Inventario
                             </a>
                             
+                            <a class="nav-link" href="inventario.php">
+                                <div class="sb-nav-link-icon"><i class="fa-solid fa-clipboard-list"></i></i></div>
+                                Factura
+                            </a>
+                            
                             <div class="collapse" id="collapsePages" aria-labelledby="headingTwo" data-bs-parent="#sidenavAccordion">
                                 <nav class="sb-sidenav-menu-nested nav accordion" id="sidenavAccordionPages">
                                     <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#pagesCollapseAuth" aria-expanded="false" aria-controls="pagesCollapseAuth">
