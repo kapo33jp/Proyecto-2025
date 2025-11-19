5f7d0f35218abe12d9f57a8e8cd329d43900b973
goso5582-afk <goso5582@gmail.com>
2025-11-18 22:37:26 -0300

    .
    .

--- html/Carrito.php
+++ html/Carrito.php
@@ -103,7 +103,7 @@ $total = 0;
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Carrito - MOCA-hairstudio</title>
-    <link rel="stylesheet" href="../Estilos/Carrito.css">
+    <link rel="stylesheet" href="../Estilos/Carrito1.css">
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
